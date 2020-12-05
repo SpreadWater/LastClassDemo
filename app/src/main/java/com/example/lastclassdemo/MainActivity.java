e9a63722b2992776728ca8057cd5d7e5d5b9d721
SpreadWater <875737147@qq.com>
2020-12-05 13:48:14 +0800

    12-05 完成主页面的逻辑

--- app/src/main/java/com/example/lastclassdemo/MainActivity.java
+++ app/src/main/java/com/example/lastclassdemo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lastclassdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.main_iv_search:
+
+                break;
+            case R.id.main_btn_edit:
+               Intent intent= new Intent(this,RecordActivity.class);
+               startActivity(intent);
+                break;
+            case R.id.main_btn_more:
+
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/lastclassdemo/RecordActivity.java
+++ app/src/main/java/com/example/lastclassdemo/RecordActivity.java
@@ -1,14 +1,57 @@
 package com.example.lastclassdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+
+import com.example.lastclassdemo.adapter.RecordPagerAdapter;
+import com.example.lastclassdemo.frag_record.IncomeFragment;
+import com.example.lastclassdemo.frag_record.OutcomeFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecordActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+        tabLayout = findViewById(R.id.record_tabs);
+        viewPager = findViewById(R.id.record_vp);
+        //设置Viewpager加载的页面
+        initPager();
+    }
+
+    private void initPager() {
+        //初始化Viewpager的集合
+        List<Fragment> fragmentList = new ArrayList<>();
+        OutcomeFragment outFrag = new OutcomeFragment();
+        IncomeFragment inFrag = new IncomeFragment();
+        fragmentList.add(outFrag);
+        fragmentList.add(inFrag);
+        //创建适配器
+        RecordPagerAdapter pagerAdapter = new RecordPagerAdapter(getSupportFragmentManager(), fragmentList);
+        //设置适配器
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    /*
+        点击事件
+     */
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.record_iv_back:
+                finish();
+                break;
+        }
     }
 }
